274cb076d71f63585ecea29684442052473627a2
Hanna <hannaeberhoefer@yahoo.com>
2021-11-29 17:20:05 +0000

    style: svg icons

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -159,8 +159,8 @@ class ListBooks extends Component {
                 </div>
               </div>
             </div>
-            <div className="open-search">
-            <Link to='/search'>Add a book</Link>
+            <div>
+              <Link to='/search' className="open-search">Add a book</Link>
             </div>
           </div>
     )
