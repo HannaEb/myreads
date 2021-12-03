68cbfaab5caeb81a538aa1cf492bc1287310873c
Hanna <hannaeberhoefer@yahoo.com>
2021-12-03 13:16:04 +0000

    feat: add update functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
 import './App.css';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then(books => {
+        this.setState(() => ({
+          books
+        }))
+      })
+  }
+
+  updateBook = (book, shelf) => {
+    BooksAPI.update(book, shelf)
+      .then(res => {
+        book.shelf = shelf;
+        this.setState(currentState => ({
+          books: currentState.books
+            .filter(currentBook => currentBook.id !== book.id)
+            .concat(book)
+        }))
+      })
   }
 
   render() {
     return ( 
       <div className="app">
         <Routes>
-        	<Route path='/' element={<ListBooks />} />
-          <Route path='/search' element={<SearchBooks />} />
+        	<Route path='/' element={<ListBooks 
+            books={this.state.books} 
+            onUpdateBook={this.updateBook} />} />
+          <Route path='/search' element={<SearchBooks 
+            books={this.state.books} 
+            onUpdateBook={this.updateBook} />} />
         </Routes>
       </div>
     )
