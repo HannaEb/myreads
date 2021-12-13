187385b906a03b7764807bcd728a5abaeeeb6719
Hanna <hannaeberhoefer@yahoo.com>
2021-12-13 11:23:05 +0000

    feat: handle missing thumbnails

--- src/Book.js
+++ src/Book.js
@@ -8,7 +8,7 @@ class Book extends Component {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks ? `url(${book.imageLinks.smallThumbnail})` : '' }}></div>
           <div className="book-shelf-changer">
             <select value={book.shelf} onChange={(event) => onUpdateBook(book, event.target.value)}>
               <option value="move" disabled>Move to...</option>
