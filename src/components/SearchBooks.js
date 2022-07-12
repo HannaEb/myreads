3cd6f0524b0ea03e1ffaaf9199be4990fb58e569
Hanna <hannaeberhoefer@yahoo.com>
2022-07-12 12:33:15 +0100

    refactor: minor adjustment

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,96 +1,82 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { debounce } from "lodash";
 import * as BooksAPI from "./../api/BooksAPI";
 import Book from "./Book";
 
-class SearchBooks extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    onUpdateBook: PropTypes.func.isRequired,
+const SearchBooks = (props) => {
+  const { books, onUpdateBook } = props;
+  const [searchTerm, setSearchTerm] = useState("");
+  const [foundBooks, setFoundBooks] = useState([]);
+  const [isError, setIsError] = useState(false);
+
+  const handleChange = (searchTerm) => {
+    updateSearchTerm(searchTerm);
+    search(searchTerm);
   };
 
-  state = {
-    searchTerm: "",
-    foundBooks: [],
-    isError: false,
+  const updateSearchTerm = (searchTerm) => {
+    setSearchTerm(searchTerm);
   };
 
-  handleChange = (searchTerm) => {
-    this.updateSearchTerm(searchTerm);
-    this.search(searchTerm);
-  };
-
-  updateSearchTerm = (searchTerm) => {
-    this.setState(() => ({
-      searchTerm: searchTerm,
-    }));
-  };
-
-  search = debounce((searchTerm) => {
+  const search = debounce((searchTerm) => {
     if (searchTerm === "") {
-      this.setState({
-        foundBooks: [],
-        isError: false,
-      });
+      setFoundBooks([]);
+      setIsError(false);
     } else {
       BooksAPI.search(searchTerm.trim()).then((foundBooks) => {
         if (!foundBooks || foundBooks.error) {
-          this.setState({
-            foundBooks: [],
-            isError: true,
-          });
+          setFoundBooks([]);
+          setIsError(true);
         } else {
-          this.setState({
-            foundBooks,
-            isError: false,
-          });
+          setFoundBooks(foundBooks);
+          setIsError(false);
         }
       });
     }
   }, 500);
 
-  render() {
-    const { searchTerm, foundBooks } = this.state;
-    const { books, onUpdateBook } = this.props;
+  foundBooks.forEach((foundBook) => {
+    let duplicate = books.find((book) => book.id === foundBook.id);
+    foundBook.shelf = duplicate ? duplicate.shelf : "none";
+  });
 
-    foundBooks.forEach((foundBook) => {
-      let duplicate = books.find((book) => book.id === foundBook.id);
-      foundBook.shelf = duplicate ? duplicate.shelf : "none";
-    });
-
-    return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link to="/" className="close-search">
-            Close
-          </Link>
-          <div className="search-books-input-wrapper">
-            <input
-              type="text"
-              placeholder="Search by title or author"
-              value={searchTerm}
-              onChange={(event) => this.handleChange(event.target.value)}
-            />
-          </div>
-        </div>
-        <div className="search-books-results">
-          <ol className="books-grid">
-            {this.state.isError ? (
-              <p>Sorry, no books found</p>
-            ) : (
-              foundBooks.map((foundBook) => (
-                <li key={foundBook.id}>
-                  <Book book={foundBook} onUpdateBook={onUpdateBook} />
-                </li>
-              ))
-            )}
-          </ol>
+  return (
+    <div className="search-books">
+      <div className="search-books-bar">
+        <Link to="/" className="close-search">
+          Close
+        </Link>
+        <div className="search-books-input-wrapper">
+          <input
+            type="text"
+            placeholder="Search by title or author"
+            value={searchTerm}
+            onChange={(event) => handleChange(event.target.value)}
+          />
         </div>
       </div>
-    );
-  }
-}
+      <div className="search-books-results">
+        <ol className="books-grid">
+          {isError ? (
+            <p>Sorry, no books found</p>
+          ) : (
+            foundBooks.map((foundBook) => (
+              <li key={foundBook.id}>
+                <Book book={foundBook} onUpdateBook={onUpdateBook} />
+              </li>
+            ))
+          )}
+        </ol>
+      </div>
+    </div>
+  );
+};
+
+SearchBooks.propTypes = {
+  books: PropTypes.array.isRequired,
+  onUpdateBook: PropTypes.func.isRequired,
+};
 
 export default SearchBooks;
